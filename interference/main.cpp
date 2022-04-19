2f39889402688763e6c78a976cc3f9a4263ee74b
Mustafa Quraish <mustafaq9@gmail.com>
2022-04-19 01:23:00 -0400

    More patterns for interference

--- interference/main.cpp
+++ interference/main.cpp
@@ -6,12 +6,15 @@
 
 #include "fleshgrad.h"
 #include "shortgold.h"
+#include "orangeblackpurple.h"
+#include "threecolor.h"
 
 #define DEBUG false
 
 struct Point2D {
     int x;
     int y;
+    float scale;
 };
 
 // Override base class with your custom functionality
@@ -35,7 +38,7 @@ public:
 		int iters = 100;
 
         if (GetMouse(0).bPressed) {
-            points.push_back({GetMouseX(), GetMouseY()});
+            points.push_back({GetMouseX(), GetMouseY(), scale});
         } else if (GetMouse(1).bPressed) {
             points.pop_back();
         } else if (GetKey(olc::Key::SPACE).bPressed) {
@@ -45,9 +48,13 @@ public:
         } else if (GetKey(olc::Key::LEFT).bPressed) {
             pattern = (pattern + 1) % 2;
         } else if (GetKey(olc::Key::UP).bPressed) {
-            color = (color + 1) % 3;
+            color = (color + 1) % 5;
         } else if (GetKey(olc::Key::DOWN).bPressed) {
-            color = (color + 2) % 3;
+            color = (color + 4) % 5;
+        } else if (GetKey(olc::Key::Z).bPressed) {
+            scale += 0.05f;
+        } else if (GetKey(olc::Key::X).bPressed) {
+            scale -= 0.05f;
         }
 
         const float SIN_SCALE = 0.1f;
@@ -60,18 +67,18 @@ public:
                     float dy = p.y - y;
                     float d = sqrt(dx * dx + dy * dy);
                     if (pattern == 0) {
-                        total += sin(d * SIN_SCALE) / N;
+                        total += sin(d * p.scale) / N;
                     } else if (pattern == 1) {
-                        total += tan(d * SIN_SCALE) / N;
+                        total += tan(d * p.scale) / N;
                     }
                 }
                 float dx = GetMouseX() - x;
                 float dy = GetMouseY() - y;
                 float d = sqrt(dx * dx + dy * dy);
                 if (pattern == 0) {
-                    total += sin(d * SIN_SCALE) / N;
+                    total += sin(d * scale) / N;
                 } else {
-                    total += tan(d * SIN_SCALE) / N;
+                    total += tan(d * scale) / N;
                 }
 
                 total += 1;
@@ -84,6 +91,10 @@ public:
                     col = fleshgrad[std::min(255, std::max(0, (int)total))];
                 } else if (color == 2) {
                     col = shortgold[std::min(255, std::max(0, (int)total))];
+                } else if (color == 3) {
+                    col = orangeblackpurple[std::min(255, std::max(0, (int)total))];
+                } else if (color == 4) {
+                    col = threecolor[std::min(255, std::max(0, (int)total))];
                 }
                 Draw(x, y, col);
             }
@@ -94,13 +105,22 @@ public:
 
     int pattern = 0;
     int color = 0;
+    float scale = 0.1;
     std::vector<Point2D> points;
 };
 
 int main()
 {
 	Example demo;
-	if (demo.Construct(256, 256, 4, 4))
+
+    printf("- Click to add a point.\n");
+    printf("- Right click to remove previous point.\n");
+    printf("- Space to clear all points.\n");
+    printf("- Z/X to change frequency of point.\n");
+    printf("- left/right to change between sin/tan.\n");
+    printf("- up/down to change color mappings.\n");
+
+	if (demo.Construct(512, 512, 2, 2))
 		demo.Start();
 	return 0;
 }
